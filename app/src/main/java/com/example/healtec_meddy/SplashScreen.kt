db21ec9de066db03466310b9595d0f822497a471
yuda <yudawirapratama05@gmail.com>
2023-04-28 23:00:29 -0700

    UPDATE SPALSH SCREEN

--- app/src/main/java/com/example/healtec_meddy/SplashScreen.kt
+++ app/src/main/java/com/example/healtec_meddy/SplashScreen.kt
@@ -1,24 +1,11 @@
 package com.example.healtec_meddy
 
-import android.content.Intent
-import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class SplashScreen : AppCompatActivity() {
-
-    private val SPLASHTIMEOUT:Long = 5000L
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
-        supportActionBar?.hide()
-
-
     }
-}
-
-private fun Handler.postDelayed(function: () -> Unit) {
-
 }
