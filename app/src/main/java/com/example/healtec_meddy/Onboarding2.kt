2428ac56ebcd6598dfe6cfebfad4bf8dd0ed4679
yuda <yudawirapratama05@gmail.com>
2023-05-02 11:38:24 -0700

    UPDATE SPALSH SCREEN 1

---
+++ app/src/main/java/com/example/healtec_meddy/Onboarding2.kt
@@ -0,0 +1,11 @@
+package com.example.healtec_meddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Onboarding2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding2)
+    }
+}
